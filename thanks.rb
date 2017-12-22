cafd990c8e296965ca0ce3381044f817bcd7bca7
Jimmy McDermott <mcdappdev@gmail.com>
2017-12-22 13:09:20 -0600

    update formula again

--- thanks.rb
+++ thanks.rb
@@ -1,10 +1,20 @@
 class Thanks < Formula
+  desc "Thank Swift packages"
   homepage "https://github.com/mcdappdev/thanks"
-  version "1.0.1"
   url "https://github.com/mcdappdev/thanks/releases/download/1.0.1/thanks-1.0.1.tar.gz"
+  version "1.0.1"
   sha256 "d6b1528313c5e752c98687bd697bfb30864a5119a83bf600c0886725ab570a53"
 
   def install
-    bin.install "thanks"
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    system "false"
   end
 end
