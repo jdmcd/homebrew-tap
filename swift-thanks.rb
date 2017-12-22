e3f1f54ef484423168e46f7389b8dfca20022b13
Jimmy McDermott <mcdappdev@gmail.com>
2017-12-22 12:30:02 -0600

    homebrew

---
+++ swift-thanks.rb
@@ -0,0 +1,10 @@
+class SwiftThanks < Formula
+  homepage "https://github.com/mcdappdev/swift-thanks"
+  version "1.0.0"
+  url "https://github.com/mcdappdev/swift-thanks/releases/download/1.0.0/swift-thanks-1.0.0.tar.gz"
+  sha256 "a87d4825cc259e581d5bced39f6309fcde830642b8ed2c0fe13fc05c0096e6f2"
+
+  def install
+    bin.install "swift-thanks"
+  end
+end
